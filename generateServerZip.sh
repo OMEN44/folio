17c8c4ca9eb957a2498258c17b8747fab62c5387
Huon <huonswales@gmail.com>
2024-02-04 12:29:29 +1000

    Minor bug fixes

--- generateServerZip.sh
+++ generateServerZip.sh
@@ -6,6 +6,9 @@ npm run build
 
 # switch to deployment mode
 sed -i 's/VITE_API_DEV=true/VITE_API_DEV=false/' .env
+echo Updated .env file:
+cat .env
+echo 
 
 # Copy files to folder
 mkdir /home/huon/Desktop/server
@@ -17,7 +20,7 @@ rsync -a --exclude=node_modules --exclude=folio.sqlite --exclude=folio-backup.sq
 # Copy to server
 cd ~/Desktop
 ssh omen@omenmc.hopto.org -p 2024 'rm ~/server/public/assets/*'
-rsync --remove-source-files -avzh -e "ssh -p 2024" server omen@omenmc.hopto.org:/home/omen
+rsync -avzh -e "ssh -p 2024" server omen@omenmc.hopto.org:/home/omen
 
 # switch to dev mode
 cd /home/huon/WebstormProjects/folio/client
